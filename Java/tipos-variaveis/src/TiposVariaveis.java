211b42cde6cb31c148978b91438c2d556436f412
VitorToku <130801252+VitorToku@users.noreply.github.com>
2024-02-10 19:50:03 -0300

    FInal da aula TiposVariaveis

--- Java/tipos-variaveis/src/TiposVariaveis.java
+++ Java/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        /*Oito tipos de dados primitivos:
+        int     byte    short   long    float   double  boolean char 
+        Os demais são considerados OBJETOS
+        é padrão usar INT para inteiros e DOUBLE para numeros fracionários*/ 
+        
+        long numberOfStars = 1548786421354654687L; //Quando usar long, é necessário colocar um L no final (pode ser minúsculo)
+        float pi = 3.14F; //Quando usar float, é necessário colocar um F no final (pode ser minúsculo)
+
+        /*Java é fortemente tipado, ou seja, minúsculo e maiúsculo importam */
+
+        //Constante é um valor armazenado em memória que não pode ser alterado após definido
+        final int ANO_FINAL = 2012;        
     }
 }
